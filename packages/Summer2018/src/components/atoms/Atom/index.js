c59067dcc70ad3496777bdaf282c5f315bd8a6c4
ColeMorton <c.morton@panorama-berlin.com>
2018-04-25 16:21:13 +0200

    add list components

--- packages/Summer2018/src/components/atoms/Atom/index.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-
-const Atom = styled.span`
-  font-family: ${props => props.theme.fonts.primary};
-`
-
-export default Atom
---
+++ packages/Summer2018/src/components/atoms/ListContent/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { List } from 'semantic-ui-react'
+
+export default List.Content
